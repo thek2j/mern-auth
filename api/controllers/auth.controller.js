9fe3789b1b5813b341fa121d32cb819e041ab384
thek2j <keyurbaraiya4@gmail.com>
2025-05-30 12:02:07 +0530

    add middleware and function to hadle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   const hashedPassword = await bcryptjs.hash(password, 10);
@@ -12,6 +12,6 @@ export const signUp = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
